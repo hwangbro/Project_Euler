7a39cdd0ab46ffda55e4dbdea46a6d018007223c
Nick Hwangbo <nickhwangbo@gmail.com>
2016-12-12 17:39:22 -0800

    Finished problem 11

--- p11/problem_11.py
+++ p11/problem_11.py
@@ -11,7 +11,12 @@ def main():
 	for line in totalf:
 		grid.append([int(i) for i in line.split(' ')])
 
-	print()
+	h = hmax(grid)
+	v = vmax(grid)
+	d = dmax(grid)
+	m = max([h,v,d])
+	print((h,v,d))
+	print(m)
 
 def prod(numlist):
 	answer = numlist[0]
@@ -33,11 +38,38 @@ def vmax(grid):
 	for i in range(20):
 		for j in range(4,20):
 			a = []
-			a.append(grid[i][j-4])
-			a.append(grid[i][j-3])
-			a.append(grid[i][j-2])
-			a.append(grid[i][j-1])
+			a.append(grid[j-4][i])
+			a.append(grid[j-3][i])
+			a.append(grid[j-2][i])
+			a.append(grid[j-1][i])
 			x = prod(a)
 			if x > m:
 				m = x
+	return m
+
+def dmax(grid):
+	m = 0
+	for i in range(4,20):
+		for j in range(4,20):
+			a = []
+			a.append(grid[i-4][j-4])
+			a.append(grid[i-3][j-3])
+			a.append(grid[i-2][j-2])
+			a.append(grid[i-1][j-1])
+			x = prod(a)
+			if x > m:
+				m = x
+	
+	for i in range(3,20):
+		for j in range(3,20):
+			a = []
+			a.append(grid[i-3][j])
+			a.append(grid[i-2][j-1])
+			a.append(grid[i-1][j-2])
+			a.append(grid[i][j-3])
+			x = prod(a)
+			if x > m:
+				m = x
+
+	return m
 main()	
