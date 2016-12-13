c1c13b00d0215ac8bf1571e268aad4de77c2e564
Nick Hwangbo <nickhwangbo@gmail.com>
2016-12-12 16:17:56 -0800

    Finished problem 8

--- p8/problem_8.py
+++ p8/problem_8.py
@@ -1,4 +1,27 @@
 import math
 
+filename = 'p8_input.txt'
+
 def main():
+	totalf = []
+	with open(filename, 'r') as f:
+		for line in f:
+			totalf.append(line)
 	
+	allnum = []
+	for line in totalf:
+		allnum += [int(i) for i in line.strip()]
+	
+	index = 13
+	m = 0
+	while index < 1001:
+		prod = allnum[index-13:index]
+		currmax = prod[0]
+		for num in prod[1:]:
+			currmax *= num
+		if currmax > m:
+			m = currmax
+		index += 1
+		print('index is ' + str(index))
+	print(m)
+main()	
